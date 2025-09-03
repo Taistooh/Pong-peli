da67015981d3adb8c1258ad79596a13c3fe18601
Tanja Heino <varjorautias@gmail.com>
2025-09-03 14:12:57 +0300

    gameover

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ const ctx = canvas.getContext('2d');
 // Kimpoaa pelaajan mailasta
 
 // Jos pallo menee vasemman reunan ohi -> peli ohi
+    document.getElementById('gameOver').style.display = 'block';
 
 // Kimpoaa oikeasta reunasta
 
