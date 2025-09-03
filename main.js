3c140a062587fa8068dfc23d497c02edd7f38155
Tanja Heino <varjorautias@gmail.com>
2025-09-03 13:38:42 +0300

    rakennetta

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
 const canvas = document.getElementById('pelialue');
 const ctx = canvas.getContext('2d');
 
+// Pallo
+
+// Pelaajan "maila"
+
+// Näppäimet
+
+// Kimpoaa ylä- ja alaseinästä
+
+// Kimpoaa pelaajan mailasta
+
+// Jos pallo menee vasemman reunan ohi -> peli ohi
+
+// Kimpoaa oikeasta reunasta
+
+// Pisteenlasku
+
